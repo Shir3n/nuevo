28fbec75a8defe322588583d239d46f1138ecd7d
Shir3n <ignacio.salazar.lopez@gmail.com>
2025-04-21 13:32:42 -0400

    avance de la clase de arreglos

---
+++ test.py
@@ -0,0 +1,6 @@
+frutas = ["manzana", "pera" , "frutilla"]
+print(frutas[2]) 
+
+estudiantes = [{"nombre": "maxi" , "edad" : 20, "curso" :"2A"}{"nombre": "richard", "edad": 19, "curso" : "1B"}]
+print(estudiantes)
+
